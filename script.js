8c30f20e0a0ea1744eea1fd877a218744086c39e
vinay1723 <144577819+vinay1723@users.noreply.github.com>
2025-06-17 18:43:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
-let url_str = Location.href.trim();
+let url_str = document.location.href;
 
-
+// alert(url_str)
 alert(`The length of the URL is: ${url_str.length}`);
 
